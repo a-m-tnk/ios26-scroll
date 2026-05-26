05fb9dce32a987ebdc2be165101487584150ab91
a.m.marchenko <a.m.marchenko@tinkoff.ru>
2026-05-26 13:45:13 +0300

    verstka

--- src/App.tsx
+++ src/App.tsx
@@ -15,33 +15,36 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React App</h1>
-      </header>
-      <div className="App-body">
-        <div className="scrollable-content">
-          <div className="content-item">
-            <h3>Content Item 0</h3>
-            <p>
-              <MobileInputLocation
-                opened={isModalOpen}
-                setOpened={setIsModalOpen}
-              />
-            </p>
-          </div>
-          {/* Adding a lot of content to make it scrollable */}
-          {[...Array(50)].map((_, i) => (
-            <div key={i} className="content-item">
-              <h3>Content Item {i + 1}</h3>
+    <div className="application">
+      <div className="Layout">
+        <header className="header">
+          <h1>React App</h1>
+        </header>
+        <div className="Page">
+          <div className="pageComponentBase">
+            <div className="content-item">
+              <h3>Content Item 0</h3>
               <p>
-                This is some sample content to demonstrate scrolling. Lorem
-                ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+                <MobileInputLocation
+                  opened={isModalOpen}
+                  setOpened={setIsModalOpen}
+                />
               </p>
             </div>
-          ))}
+            {/* Adding a lot of content to make it scrollable */}
+            {[...Array(50)].map((_, i) => (
+              <div key={i} className="content-item">
+                <h3>Content Item {i + 1}</h3>
+                <p>
+                  This is some sample content to demonstrate scrolling. Lorem
+                  ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris.
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
